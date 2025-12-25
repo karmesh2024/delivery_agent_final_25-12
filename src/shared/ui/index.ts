6988eefbb2e66a6aecdc7c2a8d164c612c00c21c
karmesh2024 <mamdouhremony@gmail.com>
2025-12-25 23:31:34 +0200

    Remove collapsible export - package not installed

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -6,7 +6,7 @@ export * from './badge';
 export * from './button';
 export * from './card';
 export * from './checkbox';
-export * from './collapsible';
+// export * from './collapsible'; // Removed - package not installed
 export * from './custom-dialog';
 export * from './dialog';
 export * from './dropdown-menu';
